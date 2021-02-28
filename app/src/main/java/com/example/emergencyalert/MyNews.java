a38064a9272da086617eede2c3968363c5e0218e
Stephen Stranahan <stephenstranahan@gmail.com>
2021-02-28 15:59:36 -0500

    Working on news feed

--- app/src/main/java/com/example/emergencyalert/MyNews.java
+++ app/src/main/java/com/example/emergencyalert/MyNews.java
@@ -1,5 +1,6 @@
 package com.example.emergencyalert;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MyNews extends Fragment {
 
@@ -29,5 +39,69 @@ public class MyNews extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                URL myUrl = null;
+                URLConnection myUrlConnection = null;
+                InputStreamReader myInputStreamReader = null;
+                BufferedReader in;
+
+                try {
+                    myUrl = new URL("https://newsapi.org/v2/top-headlines?country=us&apiKey=2ad130f782ac4c8ea47427d709022569");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    myUrlConnection = myUrl.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                java.io.InputStream myInputStream = null;
+                try {
+                    myInputStream = myUrlConnection.getInputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                myInputStreamReader = new InputStreamReader(myInputStream);
+
+
+                in = new BufferedReader(myInputStreamReader);
+
+                String line, str;
+                str = "";
+
+                System.out.print("\n");
+
+                try {
+                    while ((line = in.readLine()) != null)
+                        str += line;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                JSONObject myJsonObject = null;
+                try {
+                    myJsonObject = new JSONObject(str);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JSONArray resultsArray = null;
+                try {
+                    resultsArray = myJsonObject.getJSONArray("articles");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("\nNumber of results: " + resultsArray.length());
+
+            }
+        });
     }
 }
